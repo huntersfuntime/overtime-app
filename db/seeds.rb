b22ee03bfc3246deb225068a108a94159ef44636
huntersfuntime <hunterorges@gmail.com>
2017-03-22 15:11:17 -0600

    Refactored index page to use Post partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
- @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    
-puts "1 User Created"
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "1 User created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
