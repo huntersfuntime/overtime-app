28f53bf924dfe788dfa7d0014cbf7dcbb33fde67
huntersfuntime <hunterorges@gmail.com>
2017-04-10 11:37:30 -0600

    built instant approval functionality for employees

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: [:new, :edit, :destroy] 
-
+  resources :audit_logs, except: [:new, :edit, :destory] do
+    member do
+      get :confirm
+    end
+  end
+  
   namespace :admin do
     resources :users
-resources :admin_users
-resources :posts
+    resources :posts
+    resources :admin_users
 
     root to: "users#index"
   end
 
-  resources :posts do 
-    member do 
+  resources :posts do
+    member do
       get :approve
     end
   end
+
   devise_for :users, skip: [:registrations]
   root to: 'static#homepage'
 end
